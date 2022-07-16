c849eb40e9c712615e15e798bba1f9a6eb5f163a
Umidjon017 <81345606+Umidjon017@users.noreply.github.com>
2022-07-16 11:03:43 +0500

    project uploaded

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Write Your Code..
+     *
+     * @return string
+    */
+    public function index()
+    {
+        $posts = Post::get();
+        return view('post.index',compact('posts'));
+    }
+    /**
+     * Write Your Code..
+     *
+     * @return string
+    */
+    public function create()
+    {
+        return view('post.create');
+    }
+    /**
+     * Write Your Code..
+     *
+     * @return string
+    */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        Post::create($input);
+        return redirect()->route('posts.index');
+    }
+    /**
+     * Write Your Code..
+     *
+     * @return string
+    */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        return view('post.show',compact('post'));
+    }
+}
